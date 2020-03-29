955e1372ed6ff7d9c3f0cb4060631d87209f1670
jordy <karazejo@gmail.com>
2020-03-29 12:02:52 -0500

    more changes to

---
+++ src/contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const PatientRecordContract = require('./lib/patient-record-contract');
+
+module.exports.PatientRecordContract = PatientRecordContract;
+module.exports.contracts = [ PatientRecordContract ];
